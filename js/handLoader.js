56b92746a5ce85abf6f8d5d193dedc3866097f63
Ashish Siddharth <51298893+ashishsiddharth@users.noreply.github.com>
2019-09-25 02:38:32 +0530

    Update handLoader.js

--- js/handLoader.js
+++ js/handLoader.js
@@ -37,7 +37,7 @@ function init() {
 
   var material = new THREE.MeshPhongMaterial( { color: 0x0e2045, specular: 0x111111, shininess: 200 } );
   //loader.load( 'https://s3.amazonaws.com/limbforgestls/EbeArm/Ebe_forearm_L/forearm_L_C4-200_L1-230.stl', function ( geometry ) {
-   loader.load( 'https://github.com/kaitlynhova/Three.js-STL-Viewer/blob/master/models/hay.stl', function ( geometry ) {
+   loader.load( './models/hay.stl', function ( geometry ) {
     var mesh = new THREE.Mesh( geometry, material );
 
     mesh.position.set( 0, 0, 0 );
